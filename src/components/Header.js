9fb2ec75fd3d206cb98ffca2b7fb9063ad552d70
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-05-14 20:26:53 +0300

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="headerComp">
+            <h4 className="headText">Memory Game</h4>
+        </div>
+    )
+}
+
+export default Header;
