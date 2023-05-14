cfb9fdcba407e259cf79a7d75e034cef14455924
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-05-14 21:18:14 +0300

    Add card-container base styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import HowTo from './components/HowTo';
+import CardContainer from './components/CardContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HowTo />
+      <CardContainer />
     </div>
   );
 }
---
+++ src/components/CardContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CardContainer.css'
+
+const CardContainer = () => {
+    return (
+        <div className="card-container">
+            
+        </div>
+    )
+}
+
+export default CardContainer;
