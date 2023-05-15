69639479993d63429ee10bbeb3f2b74b00cac908
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-05-15 22:54:05 +0300

    setup card + add cat images

--- src/components/containerComponents/Card.js
+++ src/components/containerComponents/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
-import './Card.css'
+import './Card.css';
 
 const Card = () => {
     return (
         <div className="card">
-            
+            <img src={require('../../assets/cats/cat-8.png')} alt="cat_pic" />
         </div>
     )
 }
