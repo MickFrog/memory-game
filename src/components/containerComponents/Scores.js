018f7bfd32c5a9730710607caa80bd110f1e9756
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-05-18 23:12:21 +0300

    add scores logic + remove max score

--- src/components/containerComponents/Scores.js
+++ src/components/containerComponents/Scores.js
@@ -1,12 +1,11 @@
 import React from "react";  
 import './Scores.css';
 
-const Scores = () => {
+const Scores = (props) => {
     return (
         <div className="scores-div">
-            <div className="score-card max-score">Max Score: 30</div>
-            <div className="score-card current-score">Current Score: 30</div>
-            <div className="score-card best-score">Best Score: 30</div>            
+            <div className="score-card current-score">Current Score: {props.current}</div>
+            <div className="score-card best-score">Best Score: {props.best}</div>            
         </div>
     )
 }
