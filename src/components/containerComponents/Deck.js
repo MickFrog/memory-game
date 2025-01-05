d96fddcccc2930d220a24fc2565e7a5e5d3b45ef
MickFrog <107855646+MickFrog@users.noreply.github.com>
2025-01-05 12:30:35 +0900

    do code cleaning on Deck

--- src/components/containerComponents/Deck.js
+++ src/components/containerComponents/Deck.js
@@ -5,26 +5,16 @@ import Card from "./Card";
 const Deck = ({ randoms, clickFunc, updateScores }) => {
   return (
     <div className="card-deck">
-      <Card
-        picNum={randoms[0]}
-        handleClick={clickFunc}
-        updateScores={updateScores}
-      />
-      <Card
-        picNum={randoms[1]}
-        handleClick={clickFunc}
-        updateScores={updateScores}
-      />
-      <Card
-        picNum={randoms[2]}
-        handleClick={clickFunc}
-        updateScores={updateScores}
-      />
-      <Card
-        picNum={randoms[3]}
-        handleClick={clickFunc}
-        updateScores={updateScores}
-      />
+      {randoms.map((num, index) => {
+        return (
+          <Card
+            key={index}
+            picNum={num}
+            handleClick={clickFunc}
+            updateScores={updateScores}
+          />
+        );
+      })}
     </div>
   );
 };
